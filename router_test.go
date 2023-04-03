b405efc535a97854c0ca2944f8a3eed692c717cd
taufiqkba <taufiqkurniawan9991@gmail.com>
2023-04-03 13:03:20 +0700

    HTTP Router

---
+++ router_test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"github.com/julienschmidt/httprouter"
+	"github.com/stretchr/testify/assert"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestRouter(t *testing.T) {
+	router := httprouter.New()
+	router.GET("/", func(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+		fmt.Fprint(writer, "Hello GET Method")
+	})
+
+	request := httptest.NewRequest(http.MethodGet, "https://localhost:3000/", nil)
+	recorder := httptest.NewRecorder()
+
+	router.ServeHTTP(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+
+	assert.Equal(t, "Hello GET Method", string(body))
+
+}
