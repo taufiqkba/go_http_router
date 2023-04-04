aaa7e7fa8b69111b9767bf126f61c05fca23247a
taufiqkba <taufiqkurniawan9991@gmail.com>
2023-04-04 14:29:01 +0700

    Not Found Handler

---
+++ not_found_handler_test.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"github.com/julienschmidt/httprouter"
+	"github.com/stretchr/testify/assert"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestNotFoundHanlder(t *testing.T) {
+	router := httprouter.New()
+	router.NotFound = http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writer, "Not Found 404")
+	})
+
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:3000", nil)
+	recorder := httptest.NewRecorder()
+
+	router.ServeHTTP(recorder, request)
+
+	response := recorder.Result().Body
+	body, _ := io.ReadAll(response)
+
+	assert.Equal(t, "Not Found 404", string(body), "Not Found Handler Success")
+}
